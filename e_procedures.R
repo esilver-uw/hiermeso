44744065fba3eb8d0b7460b7235504a3e6d04c7c
Erin Silver <e4silver@uwaterloo.ca>
2026-07-27 20:32:41 -0400

    Restructure of the folder + all e-value procedures.

--- e_procedures.R
+++ e_procedures.R
@@ -2,64 +2,147 @@
 
 source("utils.R")
 
-# Performs eLP procedure on a subset of the groups.
+# Given a group and the two populations, return a p-value.
 # INPUT:
-# A1/A2: Observation adjacency matrix stacks
-# groups: Attribute list of all groups
-# groups_subset: List of groups to perform eLP on
-# size: an estimate/oracle value of perturbation size. if not provided, calibrate a p_value, 
-# if provided, use direct e-value method.
+# A.1: array of Adjacency Matrices from the first population.
+# A.2: array of Adjacency Matrices from the second population.
+# groups: list of group attributes.
+# g: res_Group identifier matching a group in groups.
 # OUTPUT: 
-# result: a data frame with two columns for groups and their associated e-values
-proc_elp <- function(A1, A2, groups, groups_subset, size = F) {
-  # Get e_values
-  n.groups <- length(groups_subset)
+# p_value: the p_value associated with the group-wise null hypothesis that A.1 and A.2 are generated from the same parameter matrix.
+p_value <- function(A1, A2, groups, g) {
+  # Get vector of edges in the group
+  res_grp <- data.frame(groups[[4]][groups[[4]]$res_Group == g,1:2])
+  edges <- which(groups[[1]][,res_grp$Resolution] == res_grp$Group_Number)
+  # Yield node pairs of the edges in question
+  n <- length(edges)
   
-  e_vals <- rep(NA, n.groups)
-  if (size) {
-    for (i in 1:n.groups) {
-      delta <- get_delta(size, groups, groups_subset[i])
-      e_vals[i] <- e_value_dir(A1, A2, groups, groups_subset[i], delta)
-    }
-  } else {
-    for (i in 1:n.groups) {
-      p_val <- p_value(A1, A2, groups, groups_subset[i])
-      e_vals[i] <- e_value_cal(p_val)
-    }
-  }
+  # Because groups are homogeneous across adjacency matrices, we can simply pool both sample sizes (I think?).
+  m <- n * dim(A1)[3]
+  A1_bar <- mean(apply(A1, c(1,2), mean)[edges])
+  A2_bar <- mean(apply(A2, c(1,2), mean)[edges]) # For some reason the old formulation didn't work. Probably some silly indexing.
   
-  # TODO: fix for subsets
-  selections <- elp(e_vals, groups, 0.05)
-  result <- data.frame("Selections" = selections, "e-values" = e_vals)
+  z_stat <- (A1_bar - A2_bar) * sqrt(m) / SIGMA
+  p_value <- pnorm(z_stat)
   
-  return(result)
+  return(p_value)
 }
 
-# Performs eLP procedure starting with the largest resolution.
-proc_seq_elp <- function(A1, A2, groups, size = F) {
-  resolutions <- unique(groups[[4]]$Resolution)
-  significant <- T
-  i = 1
-  selections_matrix <- data.frame()
-  
-  while(significant) {
-    groups_subset <- groups[[4]][groups[[4]]$Resolution == resolutions[i]]
-    
-    result <- proc_elp(A1, A2, groups, groups_subset, size)
-    selections <- result$Selections
-    if (sum(selections) > 0) {
-      selections_matrix[,resolutions[i]] <- selections
-    }
+# Calibrate p_value to e_value using a kappa parameter.
+# INPUT:
+# p_value: a p_value, perhaps associated with the group-wise null hypothesis that A.1 and A.2 are generated from the same parameter matrix.
+# k: power form parameter
+# OUTPUT: 
+# e_value: an e_value calibrated from that p_value.
+cal_kappa <- function(p_value, k) {
+  if (p_value > 0 & p_value < 1) {
+    e_value <- k*p_value^(k-1)
+  } else if (p_value == 0) {
+    e_value <- 1e+15
+  } else if (p_value == 1) {
+    e_value <- 0.5
   }
+  
+  # Threshold cutoff
+  if (e_value >= 1e+15) {
+    e_value <- 1e+15
+  }
+  
+  return(e_value)
 }
 
-theta_prime <- perturb_parameter_matrix(theta = THETA, groups = GROUPS, g = "res_3_group_2", size = 2)
+# Calibrate p_value to e_value.
+# INPUT:
+# p_value: a p_value, perhaps associated with the group-wise null hypothesis that A.1 and A.2 are generated from the same parameter matrix.
+# OUTPUT: 
+# e_value: an e_value calibrated from that p_value.
+cal_mixture <- function(p_value) {
+  if (p_value > 0 & p_value < 1) {
+    num <- 1 - p_value + p_value * log(p_value)
+    denom <- p_value * (-log(p_value))^2
+    e_value <- num/denom
+  } else if (p_value == 0) {
+    e_value <- 1e+15
+  } else if (p_value == 1) {
+    e_value <- 0.5
+  }
+  
+  # Threshold cutoff
+  if (e_value >= 1e+15) {
+    e_value <- 1e+15
+  }
+  
+  return(e_value)
+}
 
-A1 <- sample_network(theta = THETA, N)
-A2 <- sample_network(theta = theta_prime, N)
+# Elicit a direct e-value using a delta parameter derived from the mean.
+# INPUT: 
+# A.1: array of Adjacency Matrices from the first population.
+# A.2: array of Adjacency Matrices from the second population.
+# groups: list of group attributes.
+# g: res_Group identifier matching a group in groups.
+# pt: prior point estimate for the mean.
+# OUTPUT: 
+# e_value: an e_value.
+lr_delta <- function(A1, A2, groups, g, pt) {
+  # Get vector of edges in the group
+  res_grp <- data.frame(groups[[4]][groups[[4]]$res_Group == g,1:2])
+  edges <- which(groups[[1]][,res_grp$Resolution] == res_grp$Group_Number)
+  # Yield node pairs of the edges in question
+  n <- length(edges)
+  
+  # Because groups are homogeneous across adjacency matrices, we can simply pool both sample sizes (I think?).
+  # Use to create d.
+  m <- n * dim(A1)[3]
+  d <- pt * sqrt(m)
+  A1_bar <- mean(apply(A1, c(1,2), mean)[edges])
+  A2_bar <- mean(apply(A2, c(1,2), mean)[edges]) # For some reason the old formulation didn't work. Probably some silly indexing.
+  
+  z_stat <- (A1_bar - A2_bar) * sqrt(m) / SIGMA
+  
+  e_value <- exp(d * z_stat - d^2/2) + exp(-d * z_stat - d^2/2)
+  
+  # Threshold cutoff
+  if (e_value >= 1e+15) {
+    e_value <- 1e+15
+  }
+  
+  return(e_value)
+}
 
-E_VALS <- proc_elp(A1, A2, GROUPS, GROUPS[[4]]$res_Group, 2)
-
-selections <- elp(E_VALS, GROUPS, 0.05)
-GROUPS[[4]][which(selections == 1),]
-E_VALS[selections == 1]
+# Elicit a direct e-value using a mixture over a prior on the mean.
+# INPUT: 
+# A.1: array of Adjacency Matrices from the first population.
+# A.2: array of Adjacency Matrices from the second population.
+# groups: list of group attributes.
+# g: res_Group identifier matching a group in groups.
+# ps: prior sigma; prior variance of the mean.
+# OUTPUT: 
+# e_value: an e_value.
+lr_prior <- function(A1, A2, groups, g, ps) {
+  # Get vector of edges in the group
+  res_grp <- data.frame(groups[[4]][groups[[4]]$res_Group == g,1:2])
+  edges <- which(groups[[1]][,res_grp$Resolution] == res_grp$Group_Number)
+  # Yield node pairs of the edges in question
+  n <- length(edges)
+  
+  # Because groups are homogeneous across adjacency matrices, we can simply pool both sample sizes (I think?).
+  # Use to create d.
+  m <- n * dim(A1)[3]
+  A1_bar <- mean(apply(A1, c(1,2), mean)[edges])
+  A2_bar <- mean(apply(A2, c(1,2), mean)[edges]) # For some reason the old formulation didn't work. Probably some silly indexing.
+  
+  x_bar <- (A1_bar - A2_bar)
+  norm_term <- 1/(ps*sqrt(n/SIGMA^2 + 1/ps^2))
+  expo_term <- (m^2*x_bar)/(2*SIGMA^4*sqrt(n/SIGMA^2 + 1/ps^2))
+  
+  # combine into e-value.
+  e_value <- norm_term * exp(expo_term)
+  
+  # Threshold cutoff
+  if (e_value >= 1e+15) {
+    e_value <- 1e+15
+  }
+  
+  return(e_value)
+}
