48883739cc58f47a8ec7b98031128dc99c9339b3
esilver-uvm <esilver6@uvm.edu>
2026-08-05 21:17:32 -0400

    unsubstantial modifications to sims.

--- sims.R
+++ sims.R
@@ -2,18 +2,22 @@
 
 source("sim_utils.R")
 
-JOB.NUM <- 2
+JOB.NUM <- 3
 
-sims_array <- readRDS(paste0("outputs/sim_arrays_", JOB.NUM, "_vm.RData"))
+sim_arrays <- batchable_array(200, c("res_1_group_2", "res_2_group_2", "res_3_group_2"))
+
+saveRDS(sim_arrays, file = paste("outputs/sim_arrays_", JOB.NUM, "_vm_2.RData", sep=""))
+
+sims_array <- readRDS(paste0("outputs/sim_arrays_", JOB.NUM, "_vm_2.RData"))
 
 # Selection Arrays
 selex_array_1 <- omnibus_test(sims_array, "res_1_group_2", 0.05, 2)
 selex_array_2 <- omnibus_test(sims_array, "res_2_group_2", 0.05, 2)
 selex_array_3 <- omnibus_test(sims_array, "res_3_group_2", 0.05, 2)
 
-saveRDS(selex_array_1, "outputs/selex_array_1.RData")
-saveRDS(selex_array_2, "outputs/selex_array_2.RData")
-saveRDS(selex_array_3, "outputs/selex_array_3.RData")
+saveRDS(selex_array_1, "outputs/selex_array_1_vm_2.RData")
+saveRDS(selex_array_2, "outputs/selex_array_2_vm_2.RData")
+saveRDS(selex_array_3, "outputs/selex_array_3_vm_2.RData")
 
 # Method Arrays (Pruned methods; not all are interesting.)
 method_array_2 <- omnires_test(sims_array, 2, 0.05, 2)
@@ -27,13 +31,13 @@ method_array_9 <- omnires_test(sims_array, 9, 0.05, 2)
 method_array_10 <- omnires_test(sims_array, 10, 0.05, 2)
 # method_array_11 <- omnires_test(sims_array, 11, 0.05, 2)
 
-saveRDS(method_array_2, "outputs/method_array_2.RData")
-saveRDS(method_array_5, "outputs/method_array_5.RData")
-saveRDS(method_array_6, "outputs/method_array_6.RData")
-saveRDS(method_array_7, "outputs/method_array_7.RData")
-saveRDS(method_array_8, "outputs/method_array_8.RData")
-saveRDS(method_array_9, "outputs/method_array_9.RData")
-saveRDS(method_array_10, "outputs/method_array_10.RData")
+saveRDS(method_array_2, "outputs/method_array_2_vm_2.RData")
+saveRDS(method_array_5, "outputs/method_array_5_vm_2.RData")
+saveRDS(method_array_6, "outputs/method_array_6_vm_2.RData")
+saveRDS(method_array_7, "outputs/method_array_7_vm_2.RData")
+saveRDS(method_array_8, "outputs/method_array_8_vm_2.RData")
+saveRDS(method_array_9, "outputs/method_array_9_vm_2.RData")
+saveRDS(method_array_10, "outputs/method_array_10_vm_2.RData")
 
 # False detex rate: set signal sizes to 0 only
 sims_array_fdr <- sims_array[,,,1,,drop=F]
@@ -46,3 +50,6 @@ fdr_selex_array_1 <- omnibus_test(sims_array_fdr, "res_1_group_2", 0.05, 1, res_
 fdr_selex_array_2 <- omnibus_test(sims_array_fdr, "res_1_group_2", 0.05, 1, res_12_grps)
 fdr_selex_array_3 <- omnibus_test(sims_array_fdr, "res_1_group_2", 0.05, 1, GROUPS)
 
+saveRDS(fdr_selex_array_1, "outputs/fdr_selex_array_1_vm_2.RData")
+saveRDS(fdr_selex_array_2, "outputs/fdr_selex_array_2_vm_2.RData")
+saveRDS(fdr_selex_array_3, "outputs/fdr_selex_array_3_vm_2.RData")
